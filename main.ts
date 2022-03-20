b89154e4642c550873015e407177b3ef42ff8779
sololim <101944276+sololim@users.noreply.github.com>
2022-03-20 09:33:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+	
+})
